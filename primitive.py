a01dc6dcae3eff14bc25c15bc4d1abaf7e6ef6e6
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-09-11 18:36:12 +0500

    feat: learn numbers in python

---
+++ primitive.py
@@ -0,0 +1,13 @@
+print("======================================")
+# in JAVA, variable is a name of storage location
+# in PYTHHON, variable is named reference!
+
+count = 100
+count_type = type(count)
+print("count ", count, count_type)
+
+print(f"the count : {count}, and type ; {count_type}")
+
+result1 = count.bit_count()  # method
+result2 = count.numerator  # state
+print(result1, result2)
