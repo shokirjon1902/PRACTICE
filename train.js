5c327f082150dd256fda61085d52ec0119ea85e7
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-09-14 18:46:20 +0500

    TASK G

--- train.js
+++ train.js
@@ -1,3 +1,30 @@
+/* 
+TASK G:
+
+Yagona parametrga ega function tuzing.
+Va bu function parametr orqalik integer ma'lumot turlariga ega bo'lgan bir arrayni qabul qilsin.
+Ushbu function bizga arrayning tarkibidagi birinchi eng katta qiymatning indeksini qaytarsin.
+
+MASALAN: getHighestIndex([5, 21, 12, 21 ,8]); return qiladi 1 sonini
+Yuqoridagi misolda, birinchi indeksda 21 joylashgan.
+Va bu 21 soni arrayning tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi 1 qaytadi.
+
+*/
+
+function getHighestIndex(Yagona) {
+  let highest = Yagona[0];
+  let index = 0;
+  for (let i = 1; i < Yagona.length; i++) {
+    if (Yagona[i] > highest) {
+      highest = Yagona[i];
+      index = i;
+    }
+  }
+  return index;
+}
+
+console.log(getHighestIndex([5, 21, 12, 26, 8]));
+
 /*
 TASK F:
 
@@ -9,19 +36,19 @@ MASALAN: findDoublers("hello"); natija true qaytadi. Sababi ikki marotaba takror
 
 * */
 
-function findDoublers(a) {
-  for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < a.length; i++) {
-      if (a[i] === a[j]) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
+// function findDoublers(a) {
+//   for (let i = 0; i < a.length; i++) {
+//     for (let j = 0; j < a.length; i++) {
+//       if (a[i] === a[j]) {
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
 
-result = findDoublers("hello");
-console.log("result", result);
+// result = findDoublers("hello");
+// console.log("result", result);
 /*
 TASK E: 
 
