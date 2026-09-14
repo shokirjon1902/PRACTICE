ce51d6b833c9f2bdfae48b171b4fa0249d01cbe0
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-09-14 15:37:50 +0500

    feat: specific error handling system

--- iterable.py
+++ iterable.py
@@ -33,3 +33,22 @@ print(f"the name: {name}, hobby: {hobby} and balance: {balance}")
 del person_obj["single"]
 for key in person_obj:
     print(f"the key : {key} => value {person_obj.get(key)}")
+
+
+print("======Error handling system =======")
+
+car_dict = dict(name="Toyoto", year=2026, electric=True)
+
+try:
+    print("passed here")
+    a = car_dict.speed
+    result = car_dict["origin"]
+    print("result:", result)
+except KeyError as err:
+    print("No origin state property found:", err)
+except AttributeError as err:
+    print("No speed found:", err)
+else:
+    print("Executed successfully without errors")
+finally:
+    print("Final closing logic")
--- run.py
+++ run.py
@@ -9,7 +9,7 @@ print("result:", result)
 
 ''' In Python, there are builtin tools :
 1. TYPES > int float str list dict
-2. FUNCTIONS > print(), len(), input(), type()
+2. FUNCTIONS > print(), len(), input(), type(), str()
 3. CONSTANTS > True False None
 
  '''
