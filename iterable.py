9ebdb0d7efe2d532cf79787a47e52419c18eded4
shokirjon1902 <shokirjontojiboyev155@gmail.com>
2026-09-14 15:42:42 +0500

    fix: handle all kind of errors

--- iterable.py
+++ iterable.py
@@ -44,10 +44,8 @@ try:
     a = car_dict.speed
     result = car_dict["origin"]
     print("result:", result)
-except KeyError as err:
-    print("No origin state property found:", err)
-except AttributeError as err:
-    print("No speed found:", err)
+except Exception as err:
+    print("General Error:", err)
 else:
     print("Executed successfully without errors")
 finally:
